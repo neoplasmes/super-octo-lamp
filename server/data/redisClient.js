3d240127902bdc736139242f2a20026c9daf6811
neoplasmes <quiqqiqiu@gmail.com>
2025-03-18 13:06:49 +0300

    просто так

--- server/data/redisClient.js
+++ server/data/redisClient.js
@@ -8,6 +8,22 @@ client.on('error', err => console.log('Redis Client Error', err));
 
 client.connect().catch(err => console.log(`error while connecting to redis: ${err}`));
 
-export {
-    client
-};
+// SCAN производительнее, чем KEYS
+client.UNSAFE__findAllWithScan = async function(pattern) {
+    const matches = [];
+
+    let cursor = 0;
+
+    do {
+      const reply = await this.scan(cursor, {
+        MATCH: pattern, 
+        COUNT: 100,     
+      });
+
+      cursor = reply.cursor;
+
+      matches.push(...reply.keys);
+    } while (cursor !== 0);
+}
+
+export const redisClient = client;
